7055944313ee90e71a57892635e6624c0e499613
asitav-sen <asitav.sen@gmail.com>
2020-06-06 23:34:14 +0530

    Added dedup parameter

--- R/juggling_jaguar.R
+++ R/juggling_jaguar.R
@@ -8,13 +8,14 @@
 #' Generates combination of 2 columns from a data frame
 #' @param data.df a data frame of more than 2 columns
 #' @param rm.na is set to true by default. It removes any rows with NA
+#' @param dedup is set to true by default. It removes an rows where values of both columns are equal
 #' @return a data frame with supplied number of column, which is created from the sets by binding
 #' @example
 #' juggling_jaguar(data.frame(a= c(1), b= c(2), c= c(3), d= d(4)),m=3)
 #' @export
 #' @keywords network analysis
 # juggling jaguar juggles with columns. you need to give the data frame and number of columns to play with.
-juggling_jaguar <- function(data.df, rm.na = TRUE) {
+juggling_jaguar <- function(data.df, rm.na = TRUE, dedup= TRUE) {
   if (is.data.frame(data.df) == FALSE) {
     stop("error: data frame not supplied")
   }
@@ -50,10 +51,17 @@ juggling_jaguar <- function(data.df, rm.na = TRUE) {
   }
   #bind the data frames in the list to create a new data frame
   new.data <- data.table::rbindlist(cdata, use.names = FALSE)
+  names(new.data)[1]<-"x"
+  names(new.data)[2]<-"y"
 
   # removing na
   if (rm.na == TRUE) {
     new.data <- stats::na.omit(new.data)
+  }
+
+  # dedup
+  if(dedup == TRUE){
+    new.data<-filter(new.data,x!=y)
   }
 
   return(new.data)
